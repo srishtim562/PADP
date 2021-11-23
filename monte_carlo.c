c05b1bc7e891ed3408b5d7fbe76569325e56f6cb
srishtim562 <55397790+srishtim562@users.noreply.github.com>
2021-11-23 12:17:22 +0530

    Update monte_carlo.c

--- monte_carlo.c
+++ monte_carlo.c
@@ -3,17 +3,16 @@
 #include <stdlib.h>
 #include <math.h>
 #include <time.h>
-#define SEED 35791246
 
 void main()
 {
 	int t, n;
-	for (t = 1; t <= 8; t *= 2)
+	for (t = 1; t <= 16; t *= 2)
 	{
 		for (n = 500; n <= 2000; n += 500)
 		{
 			int i, count = 0;
-			double z, pi, x, y;
+			double z_sqr, pi, x, y;
 
 			srand(time(0));
 
@@ -31,7 +30,7 @@ void main()
 			pi = ((double)count / n) * 4;
 
 			double t2 = omp_get_wtime();
-			printf("%d Trials, %d Threads, Pi = %g in time = %lf\n", n, t, pi, t2 - t1);
+			printf("%d Trials (Input Size), %d Threads, Pi = %g in time = %lf\n", n, t, pi, t2 - t1);
 		}
 		printf("\n");
 	}
