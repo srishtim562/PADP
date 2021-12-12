a0f93711db732da8c7112f21a992050843a9ecd5
srishtim562 <55397790+srishtim562@users.noreply.github.com>
2021-12-12 17:19:36 +0530

    Update montempi.c

--- Prog1/montempi.c
+++ Prog1/montempi.c
@@ -5,33 +5,33 @@
 
 int main(int argc,char **argv)
 {
-	int iter=1000000;
-	int i,reduced_count,reduced_iter,count=0, rank;
-	double pi,x,y,z,t;
-	MPI_Init(&argc,&argv);
-	MPI_Comm_rank(MPI_COMM_WORLD,&rank);
-	if(rank==0)
-		t=MPI_Wtime();
-	if(rank>0)
+	int iter = 1000000;
+	int i, reduced_count, reduced_iter, count = 0, rank;
+	double pi, x, y, z, t;
+	MPI_Init(&argc, &argv);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	if (rank == 0)
+		t = MPI_Wtime();
+	if (rank > 0)
 	{
 		srand(time(0));
-		for(i=0;i<iter;i++)
+		for (i = 0; i < iter; i++)
 		{
-			x=(double)rand()/RAND_MAX;
-			y=(double)rand()/RAND_MAX;
-			z=x*x+y*y;
-			if(z<=1)
+			x = (double)rand() / RAND_MAX;
+			y = (double)rand() / RAND_MAX;
+			z = x * x + y * y;
+			if (z <= 1)
 				count++;
 		}	
 	}	
-	MPI_Reduce(&count,&reduced_count,1,MPI_INT,MPI_SUM,0,MPI_COMM_WORLD);
-	MPI_Reduce(&iter,&reduced_iter,1,MPI_INT,MPI_SUM,0,MPI_COMM_WORLD);
-	reduced_iter=reduced_iter-iter;
-	if(rank==0)
+	MPI_Reduce(&count, &reduced_count ,1 ,MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+	MPI_Reduce(&iter, &reduced_iter, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+	reduced_iter -= iter;
+	if (rank == 0)
 	{
-		t=MPI_Wtime()-t;
-		pi=(double)reduced_count/(double)reduced_iter * 4;
-		printf("\npi: %lf time : %lf",pi,t);
+		t = MPI_Wtime() - t;
+		pi = (double)reduced_count / (double)reduced_iter * 4;
+		printf("\npi: %lf time : %lf", pi, t);
 	}	
 	MPI_Finalize();
 	return 0;
